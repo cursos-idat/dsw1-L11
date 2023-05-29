c667e70eaa98b4e0670a9efee8518f06acc52da4
Ernesto Anaya <ernestex@gmail.com>
2023-05-29 14:01:00 -0500

    agregando Perú

--- dsw1-L11/src/main/java/pe/edu/idat/dsw1L11/CountryRepository.java
+++ dsw1-L11/src/main/java/pe/edu/idat/dsw1L11/CountryRepository.java
@@ -38,6 +38,16 @@ public class CountryRepository {
 		uk.setPopulation(63705000);
 
 		countries.put(uk.getName(), uk);
+
+		Country pe = new Country();
+		pe.setName("Peru");
+		pe.setCapital("Lima");
+		pe.setCurrency(Currency.PEN);
+		pe.setPopulation(33000001);
+
+		countries.put(pe.getName(), pe);
+
+
 	}
 
 	public Country findCountry(String name) {
